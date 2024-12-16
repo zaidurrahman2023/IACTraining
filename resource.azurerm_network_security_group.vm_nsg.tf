6d27d56b8d8412d0c7debaeb84433d886b3f8fda
Zaid ur Rahman <zaid.rahman@libertyutilities.com>
2024-12-16 15:48:46 -0500

    Enhance Azure Network Security Group configuration by adding source and destination address prefixes for SSH rule. This update improves security by explicitly defining allowed IP ranges for inbound traffic.

--- resource.azurerm_network_security_group.vm_nsg.tf
+++ resource.azurerm_network_security_group.vm_nsg.tf
@@ -3,13 +3,16 @@ resource "azurerm_network_security_group" "vm_nsg" {
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
   security_rule {
-    name                   = "SSH"
-    priority               = 1001
-    direction              = "Inbound"
-    access                 = "Allow"
-    protocol               = "Tcp"
-    source_port_range      = "*"
-    destination_port_range = "22"
+    name                         = "SSH"
+    priority                     = 1001
+    direction                    = "Inbound"
+    access                       = "Allow"
+    protocol                     = "Tcp"
+    source_port_range            = "*"
+    destination_port_range       = "22"
+    source_address_prefixes      = ["0.0.0.0/0"]
+    destination_address_prefixes = ["0.0.0.0/0"]
   }
 }
+
 
